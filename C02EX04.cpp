c446d5bfe7689b11048936584bc7b95374163eaf
Diegusto <90938379+Diegusto@users.noreply.github.com>
2022-03-28 20:00:34 -0300

    outra forma

---
+++ C02EX04.cpp
@@ -0,0 +1,24 @@
+// C02EX04.CPP
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main (void)
+{
+   char NOME[40];
+
+  cout << "Informe seu nome: ";
+  cin >> NOME;
+   cin.ignore(80, '\n');
+
+   cout << "Ola, " << endl;
+   cout << NOME <<endl;
+
+   cout << endl;
+   cout << "Tecle <Enter> para encerrar... ";
+   cin.get();
+   return 0;
+}
